ac55d19aa3c24fbbca63e10a22950a0cdec20eec
jonathanherrmannengel <38504994+jonathanherrmannengel@users.noreply.github.com>
2018-04-25 20:30:42 +0200

    color changes

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var updatedSW = 36; //TO BE INCREASED ON EACH NEW VERSION
+var updatedSW = 37; //TO BE INCREASED ON EACH NEW VERSION
 
 //generate cache-name from app-version, branch and sw-version
 var locationstr = location.pathname;

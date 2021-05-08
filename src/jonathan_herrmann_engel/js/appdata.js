e436ef97d778b40f37283c404b4a11d4c9ea4ac3
Jonathan Herrmann-Engel <38504994+jonathanherrmannengel@users.noreply.github.com>
2021-05-08 16:58:11 +0200

    update 20210508-165811

--- src/jonathan_herrmann_engel/js/appdata.js
+++ src/jonathan_herrmann_engel/js/appdata.js
@@ -3,11 +3,11 @@ const APP_DATA = {
     "version": {
         "major": 7,
         "minor": 1,
-        "patch": 3,
+        "patch": 4,
         "date": {
             "year": 2021,
-            "month": 4,
-            "day": 16
+            "month": 5,
+            "day": 3
         }
     }
 };

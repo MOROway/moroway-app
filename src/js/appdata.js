a6bc77f34c1633fa2447c520bde84c1f6987e278
jonathanherrmannengel <38504994+jonathanherrmannengel@users.noreply.github.com>
2022-02-27 10:23:54 +0000

    Auto update MOROway App to new version v7.4.1

--- src/js/appdata.js
+++ src/js/appdata.js
@@ -3,12 +3,12 @@ const APP_DATA = {
     "version": {
         "major": 7,
         "minor": 4,
-        "patch": 0,
+        "patch": 1,
         "beta": 0,
         "date": {
             "year": 2022,
             "month": 2,
-            "day": 25
+            "day": 27
         }
     },
     "platform": "web",
